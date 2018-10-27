ae97d5ec2339ddf6284489e360e8f5d9b8001cc6
electrone901 <carbajalluis@rocketmail.com>
2018-10-27 18:42:01 -0400

    add simple express server

---
+++ backend/routes/api/resources.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const Resources = require('../../models/Resources');
+
+// @route   GET api/companies
+router.get('/', (req,res) => res.send({msg: "resources Work"}));
+
+// get all
+router.get('/all', (req, res) => {
+     Resources.find({})
+         .populate('title')
+         .then(stocks => {
+             res.json(resources);
+         })
+         .catch(err => 
+             res.status(404).json({error: err})
+        );
+});
+
+// get a resource by id
+// router.get('/all/:id', (req, res) => {
+//      Stock.find({user: req.params.id})
+//          .sort({symbol: 1})
+//          .populate('user', ['name'])
+//          .then(stocks => {
+//              res.json(stocks);
+//          })
+//          .catch(err => 
+//              res.status(404).json({error: err})
+//         );
+// });
+
+// create a post
+router.post('/',(req, res) => {
+	console.log('req.body.title', req.body)
+    const resources = {};
+    resources.title = req.body.title;
+    resources.link = req.body.link;
+    resources.image = req.body.image;
+    
+    new Resources(resources).save().then(resources => res.json(resources));
+});
+
+module.exports = router;
+
